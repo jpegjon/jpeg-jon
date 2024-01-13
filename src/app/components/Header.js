8902b33387b7a17a6d7e2f50b280fba42faad53d
Jon Tsikniadopoulos <jon.tsiknias@gmail.com>
2024-01-13 09:12:29 -0800

    Create Header.js

---
+++ src/app/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex">
+      <h1>Header</h1>
+      <p>Based in Los Angeles</p>
+      <p>Available for work</p>
+    </div>
+  );
+}
+
+export default Header;
